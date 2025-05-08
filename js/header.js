5754a65743d33d2614768f1c22cdb491615b2dff
손단하 <danhana@gmail.com>
2025-05-08 13:06:50 +0900

    로그인 페이지 완료

--- js/header.js
+++ js/header.js
@@ -3,11 +3,33 @@ fetch("../header.html")
   .then((html) => {
     document.getElementById("header-placeholder").innerHTML = html;
 
-    document.getElementById("loginBtn").addEventListener("click", () => {
+    const loginPage = location.pathname.includes("login");
+    const signupPage = location.pathname.includes("signup");
+
+    const loginBtn = document.getElementById("loginBtn");
+    const signupBtn = document.getElementById("signupBtn");
+    const homeBtn = document.querySelector(".home__btn");
+    const main = document.querySelector(".main");
+
+    if ((loginPage || signupPage) && loginBtn && signupBtn && homeBtn) {
+      loginBtn.style.display = "none";
+      signupBtn.style.display = "none";
+
+      homeBtn.style.display = "inline-block";
+    }
+    if (signupPage && main) {
+      main.style.margin = "0";
+    }
+
+    homeBtn?.addEventListener("click", () => {
+      window.location.href = "/index.html";
+    });
+
+    loginBtn?.addEventListener("click", () => {
       window.location.href = "/login.html";
     });
 
-    document.getElementById("signupBtn").addEventListener("click", () => {
+    signupBtn?.addEventListener("click", () => {
       window.location.href = "/signup.html";
     });
   })
